a550c4640fe36e01a1a10be20f57e7b0e94520fe
Abdul Rafay <malickawan310aa@gmail.com>
2026-08-21 01:01:23 +0300

    Updated site URL to devicecompat.com

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,7 @@
 import { defineConfig } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
 
-// https://astro.build/config
 export default defineConfig({
-  site: 'https://smart-compatibility-pro.netlify.app',
+  site: 'https://devicecompat.com',
   integrations: [sitemap()],
 });
