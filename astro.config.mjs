7a4928e08a50a4b6ce4a14b2ad5ba998f4e7222b
Abdul Rafay <malickawan310aa@gmail.com>
2026-08-25 01:11:28 +0300

    Fix pages with redirect error

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,6 @@ import { defineConfig } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
-  site: 'www.devicecompat.com',
+  site: 'https://devicecompat.com',
   integrations: [sitemap()],
 });
